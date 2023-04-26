fa077facb33f79d4af63be0a8f3453d647f1c5ae
Jens Reimann <jreimann@redhat.com>
2023-04-26 14:49:08 +0200

    refactor: extract pkg input to prepare for different types

--- src/pages/package/lookup.rs
+++ src/pages/package/lookup.rs
@@ -9,30 +9,12 @@ const DEFAULT_SEARCH: &str = "pkg:maven/io.quarkus/quarkus-core@2.13.7.Final-red
 
 #[function_component(LookupPackageModal)]
 pub fn lookup_package_modal() -> Html {
-    use patternfly_yew::next::TextInput;
-
-    let form_state = use_state_eq(InputState::default);
-
-    let onvalidated_form = {
-        let form_state = form_state.clone();
-        Callback::from(move |state| form_state.set(state))
-    };
-
-    let validator_purl = |ctx: ValidationContext<String>| {
-        if ctx.value.is_empty() {
-            return ValidationResult::error("Must not be empty");
-        }
-
-        if let Err(err) = PackageUrl::from_str(&ctx.value) {
-            return ValidationResult::error(format!("Unable to parse as Package URL({err})"));
-        }
-
-        ValidationResult::ok()
-    };
-    let purl = use_state_eq(|| DEFAULT_SEARCH.to_string());
-
     let router = use_router::<AppRoute>();
     let backdrop = use_backdrop();
+
+    let purl = use_state_eq(|| DEFAULT_SEARCH.to_string());
+    let form_state = use_state_eq(InputState::default);
+
     let onclick = {
         let purl = purl.clone();
         let backdrop = backdrop.clone();
@@ -62,6 +44,20 @@ pub fn lookup_package_modal() -> Html {
         )
     };
 
+    let onchange = {
+        let purl = purl.clone();
+        Callback::from(move |data| {
+            purl.set(data);
+        })
+    };
+
+    let onvalidated = {
+        let form_state = form_state.clone();
+        Callback::from(move |state| {
+            form_state.set(state);
+        })
+    };
+
     html!(
         <Bullseye plain=true>
             <Modal
@@ -69,18 +65,72 @@ pub fn lookup_package_modal() -> Html {
                 variant={ModalVariant::Small}
                 {footer}
             >
-                <Form id="lookup-form" method="dialog"
-                    onvalidated={onvalidated_form}
-                >
-                    <FormGroupValidated<TextInput>
-                        label="Package URL (PURL)"
-                        required=true
-                        validator={Validator::from(validator_purl)}
-                    >
-                        <TextInput value={(*purl).clone()} oninput={Callback::from(move |data| purl.set(data))}/>
-                    </FormGroupValidated<TextInput>>
-                </Form>
+                <PurlTab {onchange} {onvalidated}/>
             </Modal>
         </Bullseye>
+    )
+}
+
+#[derive(Clone, Debug, PartialEq, Properties)]
+struct PackageLookupProperties {
+    onchange: Callback<String>,
+    onvalidated: Callback<InputState>,
+}
+
+#[function_component(PurlTab)]
+fn purl_tab(props: &PackageLookupProperties) -> Html {
+    use patternfly_yew::next::TextInput;
+
+    let form_state = use_state_eq(InputState::default);
+
+    {
+        let onvalidated = props.onvalidated.clone();
+        use_effect_with_deps(
+            move |state| {
+                onvalidated.emit(**state);
+            },
+            form_state.clone(),
+        );
+    }
+
+    let onvalidated_form = {
+        let form_state = form_state.clone();
+        Callback::from(move |state| form_state.set(state))
+    };
+
+    let validator_purl = |ctx: ValidationContext<String>| {
+        if ctx.value.is_empty() {
+            return ValidationResult::error("Must not be empty");
+        }
+
+        if let Err(err) = PackageUrl::from_str(&ctx.value) {
+            return ValidationResult::error(format!("Unable to parse as Package URL({err})"));
+        }
+
+        ValidationResult::ok()
+    };
+
+    let purl = use_state_eq(|| DEFAULT_SEARCH.to_string());
+    let oninput = {
+        let purl = purl.clone();
+        let onchange = props.onchange.clone();
+        Callback::from(move |data: String| {
+            purl.set(data.clone());
+            onchange.emit(data);
+        })
+    };
+
+    html!(
+        <Form id="lookup-form" method="dialog"
+            onvalidated={onvalidated_form}
+        >
+            <FormGroupValidated<TextInput>
+                label="Package URL (PURL)"
+                required=true
+                validator={Validator::from(validator_purl)}
+            >
+                <TextInput value={(*purl).clone()} {oninput}/>
+            </FormGroupValidated<TextInput>>
+        </Form>
     )
 }
