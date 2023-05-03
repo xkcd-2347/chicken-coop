89df031100b33239e05fb40532a11a7085490100
Jens Reimann <ctron@dentrassi.de>
2023-05-03 09:10:59 +0200

    feat: add a way to load the backend

--- Containerfile
+++ Containerfile
@@ -12,14 +12,20 @@ RUN true \
 
 FROM registry.access.redhat.com/ubi9/ubi-minimal:latest
 
-RUN microdnf install -y nginx
+LABEL org.opencontainers.image.source="https://github.com/xkcd-2347/chicken-coop"
+
+RUN microdnf install -y nginx jq
 
 RUN true \
-    && mkdir /public
+    && mkdir /public \
+    && mkdir /endpoints
 
 COPY --from=builder /public /public/
 COPY config/nginx.conf /etc/nginx/nginx.conf
 
+COPY config/nginx.sh /nginx.sh
+RUN chmod a+x /nginx.sh
+
 EXPOSE 80
 
-CMD ["/usr/sbin/nginx", "-g", "daemon off;"]
+CMD [ "/nginx.sh" ]
--- src/components/backend.rs
+++ src/components/backend.rs
@@ -1,5 +1,7 @@
+use patternfly_yew::prelude::*;
 use std::rc::Rc;
 use url::Url;
+use web_sys::RequestCache;
 use yew::prelude::*;
 use yew_more_hooks::hooks::r#async::*;
 
@@ -10,24 +12,53 @@ pub struct BackendProperties {
     pub bootstrap_url: String,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
+pub struct BackendEndpoint {
+    pub url: Url,
+}
+
 #[function_component(Backend)]
 pub fn backend(props: &BackendProperties) -> Html {
     let backend = use_async_with_options(
         async {
-            Ok::<_, ()>(crate::backend::Backend {
-                // we cannot use reqwest here, as we might need to do a relative lookup, based on the
-                // current web page. Which is something that Url (which is used by reqwest) doesn't
-                // support. But gloo_net does.
-                // FIXME: load from somewhere later on using props.bootstrap_url
-                url: Url::parse("https://api-trusted.apps.sandbox.drogue.world").unwrap(),
-            })
+            log::info!("Discovering backend...");
+            // we cannot use reqwest here, as we might need to do a relative lookup, based on the
+            // current web page. Which is something that Url (which is used by reqwest) doesn't
+            // support. But gloo_net does.
+            let response = gloo_net::http::Request::get("/endpoints/backend.json")
+                .cache(RequestCache::NoCache)
+                .send()
+                .await
+                .map_err(|err| format!("Failed to load backend information: {err}"))?;
+
+            let endpoint: BackendEndpoint = response
+                .json()
+                .await
+                .map_err(|err| format!("Failed to decode backend information: {err}"))?;
+
+            log::info!("Found: {endpoint:?}");
+
+            Ok::<_, String>(crate::backend::Backend { url: endpoint.url })
         },
         UseAsyncOptions::enable_auto(),
     );
 
     match &*backend {
         UseAsyncState::Pending | UseAsyncState::Processing => html!(),
-        UseAsyncState::Ready(Err(())) => html!(<>{"Failed to initialize backend"}</>),
+        UseAsyncState::Ready(Err(err)) => html!(
+            <Bullseye>
+                <Grid gutter=true>
+                    <GridItem offset={[2]} cols={[2]}>
+                        <img src="assets/images/chicken-svgrepo-com.svg" style="transform: scaleY(-1);"/>
+                    </GridItem>
+                    <GridItem cols={[6]}>
+                        <Title>{"Failure"}</Title>
+                        { format!("Failed to initialize backend: {err}") }
+                    </GridItem>
+                </Grid>
+
+            </Bullseye>
+        ),
         UseAsyncState::Ready(Ok(backend)) => html!(
             <ContextProvider<Rc<crate::backend::Backend>> context={Rc::new(backend.clone())}>
                 { for props.children.iter() }
