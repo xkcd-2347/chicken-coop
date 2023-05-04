f1d25bedf9338d0d4b98669ceca463745aaf734c
Jens Reimann <ctron@dentrassi.de>
2023-05-04 15:47:48 +0200

    refactor: show an empty state when there is a 404 for a vuln

--- src/backend/vuln.rs
+++ src/backend/vuln.rs
@@ -1,5 +1,6 @@
 use super::{Backend, Error};
 use crate::backend::data::Vulnerability;
+use reqwest::StatusCode;
 
 pub struct VulnerabilityService {
     backend: Backend,
@@ -14,15 +15,18 @@ impl VulnerabilityService {
         }
     }
 
-    pub async fn lookup(&self, cve: &String) -> Result<Vulnerability, Error> {
-        Ok(self
+    pub async fn lookup(&self, cve: &String) -> Result<Option<Vulnerability>, Error> {
+        let response = self
             .client
             .get(self.backend.url.join("/api/vulnerability")?)
             .query(&[("cve", cve.to_string())])
             .send()
-            .await?
-            .error_for_status()?
-            .json()
-            .await?)
+            .await?;
+
+        if response.status() == StatusCode::NOT_FOUND {
+            return Ok(None);
+        }
+
+        Ok(Some(response.error_for_status()?.json().await?))
     }
 }
